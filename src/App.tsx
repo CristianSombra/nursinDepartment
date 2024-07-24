e22a99d37a5920b5f9c4d4c54711bbe1bc7e482f
CristianLegion <108194989+CristianSombra@users.noreply.github.com>
2024-07-24 08:15:42 -0300

    Contact created.

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { Suspense } from 'react'
 import AppRouter from './routes/AppRouter'
 import { SpinnerLoader } from './components';
+import "react-toastify/dist/ReactToastify.css";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js'
 import './App.css'

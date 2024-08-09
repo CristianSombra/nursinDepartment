ae4c80067593f1a43dcfb596341c8622ecd60ca7
Cristian Sombra <cristiansombra87@gmail.com>
2024-08-09 11:54:30 -0300

    Contact update

--- src/pages/contact/Contact.tsx
+++ src/pages/contact/Contact.tsx
@@ -15,7 +15,7 @@ const Contact: React.FC<AnimateProps>= ({animate}) => {
             transition={{ duration: 0.6 }}
             >
 
-            <section className="container container-contact-title position-relative">
+            <section className="container container-contact-title position-relative title-contact">
                 <div className="row my-5 d-flex justify-content-center text-center">
                     <div className="col-12 col-md-10 col-lg-8 my-5">
                         <motion.div 
@@ -28,13 +28,13 @@ const Contact: React.FC<AnimateProps>= ({animate}) => {
                             animate={{x: 0, opacity: 1, filter: 'blur(0)'}}
                             transition={{duration: 1.2, delay: 0.4}}
                             className="blue-rectangle-contact"></motion.div>
-                        <motion.h3 
+                        <motion.h1 
                             initial={{y: -50, opacity: 0, filter: 'blur(4px)'}}
                             animate={{y: 0, opacity: 1, filter: 'blur(0)'}}
                             transition={{duration: 1.2, delay: 0.4}}
                             className="fw-bold my-3">
                                 Contacto
-                        </motion.h3>
+                        </motion.h1>
                         <motion.p
                             initial={{opacity:0, filter: 'blur(4px)'}}
                             animate={{opacity:1, filter: 'blur(0)'}}
